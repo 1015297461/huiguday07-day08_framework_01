0183bf39a4f92ead26be90200f56305ac4dcb806
chuyifei917 <1015297461@qq.com>
2020-06-11 12:28:48 +0800

    yes测试

--- run_suite.py
+++ run_suite.py
@@ -10,6 +10,8 @@ suite = unittest.TestLoader().discover("script", "test*.py")
 # 3.定义测试报告
 # report_file = "{}-report.html". format(time.strftime("%Y%m%d%H%M%S"))
 
+
 # 使用beautiful批量
-BeautifulReport(suite).report(filename="report.html", description="TpshopAPI", report_dir="./report")
+file = "report.html"
+BeautifulReport(suite).report(filename=file, description="TpshopAPI", report_dir="./report")
 
