4092d9d9bbe18bda44a6f940fed85727793ca385
chuyifei917 <1015297461@qq.com>
2020-06-11 10:21:54 +0800

    yes

--- run_suite.py
+++ run_suite.py
@@ -1,22 +1,15 @@
-import os
-import unittest
+# 1.导包
 import time
+import unittest
+from BeautifulReport import BeautifulReport
 
-import HTMLTestRunner_PY3
-from script.test_ihrm_login import TestIHRMLogin
+# 2.组织测试套件
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+suite = unittest.TestLoader().discover("script", "test*.py")
 
-# 2 创建测试套件
-suite = unittest.TestSuite()
-# 3 将测试用例添加到测试套件
-suite.addTest(unittest.makeSuite(TestIHRMLogin))
-# 4 定义测试报告的目录和报告名称
-report_path = BASE_DIR + "/report/ihrm{}.html".format(time.strftime('%Y%m%d %H%M%S'))
-# 5 使用HTMLTestRunner_PY3生成测试报告
-with open(report_path, mode='wb') as f:
-    # 实例化HTMLTestRunner_PY3
-    runner = HTMLTestRunner_PY3.HTMLTestRunner(f, verbosity=1, title="ihrm系统接口测试",
-                                               description="我们的IHRM的接口测试报告")
-    # 使用实例化的runner运行测试套件，生成测试报告
-    runner.run(suite)
+# 3.定义测试报告
+report_file = "{}-report.html". format(time.strftime("%Y%m%d%H%M%S"))
+
+# 使用beautiful批量
+BeautifulReport(suite).report(filename=report_file, description="TpshopAPI", report_dir="./report")
+
